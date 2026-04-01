9d726051d13fc1c493a08285f219309b6ed7f585
robharmer3 <rob.harmer3@gmail.com>
2026-04-01 11:06:14 +0100

    app.js port updated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res) {
-	res.write("On the way to be a full stack engineer!");
-	res.end();
-}).listen(4000);
+http
+  .createServer(function (req, res) {
+    res.write("On the way to be a full stack engineer!");
+    res.end();
+  })
+  .listen(3000);
 
-console.log("Server started on port 4000");
+console.log("Server started on port 3000");
